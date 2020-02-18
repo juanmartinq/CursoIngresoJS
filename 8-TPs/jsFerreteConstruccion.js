262b5da4aa3d3d092014d41c7f318619def57954
juanmartinq <juanmartin.q@hotmail.com>
2020-02-18 20:42:49 -0300

    Tp ferrete construcciones

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -3,14 +3,27 @@ A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el anch
 B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
 */
+var largoTerreno = parseInt(document.getElementById("Largo").value)
+var anchoTerreno = parseInt(document.getElementById("Ancho").value)
+var radioTerreno = parseInt(document.getElementById("Radio").value)
+var rectangulo
+var circulo
+var contraPiso
 
 function Rectangulo () 
 {
+    var largoTerreno = parseInt(document.getElementById("Largo").value)
+var anchoTerreno = parseInt(document.getElementById("Ancho").value)
+var radioTerreno = parseInt(document.getElementById("Radio").value)
+    var rectangulo = ((largoTerreno*2)+(anchoTerreno*2))*3;
+    alert ("Para alambrar 3 veces el terreno rectangular se necesitan comprar "+ rectangulo + " metros de alambre.")
 
 }
 function Circulo () 
 {
-	
+    var radioTerreno = parseInt(document.getElementById("Radio").value)
+    var circulo = (radioTerreno*2)*3;
+    alert ("Para alambrar 3 veces el terreno circular se deben comprar "+circulo +(" metros de alambre."));
 }
 function Materiales () 
 {
--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -4,25 +4,37 @@ B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 
-var precioUno = parseInt(document.getElementById("PrecioUno").value);
-var precioDos = parseInt(document.getElementById("PrecioDos").value);
-var precioTres = parseInt(document.getElementById("PrecioTres").value);
+
 var suma
 var promedio
 var precioFinal
 
+
+
 function Sumar () 
 {
+    
+    var precioUno = parseInt(document.getElementById("PrecioUno").value);
+var precioDos = parseInt(document.getElementById("PrecioDos").value);
+var precioTres = parseInt(document.getElementById("PrecioTres").value);
     suma = precioUno+precioDos+precioTres;
     alert("La suma del valor de los productos es: " + suma);
 }
 function Promedio () 
 {
+
+    var precioUno = parseInt(document.getElementById("PrecioUno").value);
+var precioDos = parseInt(document.getElementById("PrecioDos").value);
+var precioTres = parseInt(document.getElementById("PrecioTres").value);
     promedio = (precioUno+precioDos+precioTres) /3;
     alert("El promedio del valor de los productos es " + promedio);
 }
 function PrecioFinal () 
 {
+    
+    var precioUno = parseInt(document.getElementById("PrecioUno").value);
+var precioDos = parseInt(document.getElementById("PrecioDos").value);
+var precioTres = parseInt(document.getElementById("PrecioTres").value);
     precioFinal = (suma *0.21) +suma;
     alert("El precio final (IVA incluido) es: "+ precioFinal);
 }
